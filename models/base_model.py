483a9718c1693c97dc31bfcaa5bd6eaf2f73abac
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-03-22 14:58:55 +0200

    base model

---
+++ models/base_model.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Base model for our program"""
+
+
+from datetime import *
+import uuid
+
+
+class BaseModel:
+    """base class that
+    defines all common attributes/methods for other classes
+    """
+    def __init__(self):
+        self.id = int(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+    
+    def __str__(self):
+        return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        return self.__dict__
