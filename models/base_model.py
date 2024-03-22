4d0753c0be3667028e14eb39fff4361832511992
bahaa_mustafa <bahaamustafabadr@gmail.com>
2024-03-22 17:02:40 +0200

    ..

--- models/base_model.py
+++ models/base_model.py
@@ -11,15 +11,21 @@ class BaseModel:
     defines all common attributes/methods for other classes
     """
     def __init__(self):
-        self.id = int(uuid.uuid4())
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-
+    
     def __str__(self):
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-
+    
     def save(self):
         self.updated_at = datetime.now()
-
+    
     def to_dict(self):
-        return self.__dict__
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__': self.__class__.__name__})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+
+        return dictionary
