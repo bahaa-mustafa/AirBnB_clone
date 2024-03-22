e5aff6cd217171824d3557d45ab41f20c963deb3
Bahaa Badr <bahaamustafabadr@gmail.com>
2024-03-22 06:17:06 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -14,12 +14,12 @@ class BaseModel:
         self.id = int(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-    
+
     def __str__(self):
         return(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
-    
+
     def save(self):
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
         return self.__dict__
